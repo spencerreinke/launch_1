b764943383654083e38160946dc1ed49219bbb0c
Spencer Reinke <reinke.spencer@gmail.com>
2014-01-08 16:37:56 -0600

    Update Gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Launch1::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+root  'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
